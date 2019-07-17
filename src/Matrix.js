d828aee7b485e04ecc3f8dae388a12f2a9eaf7cf
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-17 11:44:12 -0400

    passed function from parent to child

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,23 +5,38 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.keepTrack = this.keepTrack.bind(this)
+    this.changeColor = this.changeColor.bind(this)
+    this.state = {
+      selectedColor: ""
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} change={this.changeColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  keepTrack = (event) => {
+    const color =  event.target.style.backgroundColor
+    this.setState({
+      selectedColor: color
+    })
+  }
+
+  changeColor = (event) => {
+    event.target.style.backgroundColor = this.state.selectedColor
+  }
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector track={this.keepTrack}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
